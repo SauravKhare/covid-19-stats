b4b81f79338ac22f21703145f3afb508651624c9
Saurav Khare <saurav.erahk@gmail.com>
2020-03-26 13:00:42 +0530

    Fixed Country Selector and some UI fixex

--- components/CountrySelector.js
+++ components/CountrySelector.js
@@ -15,6 +15,12 @@ const StatH2 = styled.h2`
   color: #000000;
 `;
 
+const StatH2Img = styled.h2`
+  font-size: 1.5rem;
+  color: #000000;
+  vertical-align: top;
+`;
+
 const StatP = styled.p`
   font-size: 1.5rem;
   color: #000000;
@@ -29,32 +35,44 @@ export default function CountrySelector() {
   const { stats: countries, loading, error } = useStats(
     'https://covid19.mathdro.id/api/countries'
   );
-  const [selectedCountry, setSelectedCountry] = useState('IND');
-  if (loading) return <p>Loading...</p>;
+
+  const [selectedCountry, setSelectedCountry] = useState({
+    name: 'India',
+    iso2: 'IN'
+  });
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error...</p>;
 
   return (
-    <CountryText>
-      <StatP>Select Country</StatP> <SelectCountry
-        onChange={e => {
-          setSelectedCountry(e.target.value);
-        }}
-      >
-        {Object.entries(countries.countries).map(([country, code]) => (
-          <option
-            selected={selectedCountry === countries.iso3[code]}
-            key={code}
-            value={countries.iso3[code]}
-          >
-            {country}
-          </option>
-        ))}
-      </SelectCountry>
-      <StatH2>Currently Showing From {selectedCountry}</StatH2>
+    <div>
+      <CountryText>
+      <StatP>Select Country</StatP>
+      <SelectCountry
+          onChange={e => {
+            setSelectedCountry(countries.countries[e.target.value]);
+          }}
+        >
+          {countries.countries.map((country, i) => (
+            <option
+              selected={selectedCountry.name === country.name}
+              key={country.ios2}
+              value={i}
+            >
+              {country.name}
+            </option>
+          ))}
+        </SelectCountry>
+        <div>
+          <StatH2Img>Stats for {selectedCountry.name} <img style={{verticalAlign: "middle"}}
+            src={`https://www.countryflags.io/${selectedCountry.iso2}/flat/32.png`}
+          /></StatH2Img>
+        </div>
+      </CountryText>
       <Stats
-        url={`https://covid19.mathdro.id/api/countries/${selectedCountry}`}
+        url={encodeURI(
+          `https://covid19.mathdro.id/api/countries/${selectedCountry.name}`
+        )}
       ></Stats>
-    </CountryText>
+    </div>
   );
 }
